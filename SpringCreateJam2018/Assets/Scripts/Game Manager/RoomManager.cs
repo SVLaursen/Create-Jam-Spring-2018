d24df46616fe2493691a350a088497cebf750e15
Simon Laursen <>
2018-03-23 20:26:19 +0100

    Added in scenes and started working on the scene management

---
+++ SpringCreateJam2018/Assets/Scripts/Game Manager/RoomManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviour {
+
+    private SceneManager sceneManager = new SceneManager();
+
+    public Scene[] rooms = new Scene[12];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
