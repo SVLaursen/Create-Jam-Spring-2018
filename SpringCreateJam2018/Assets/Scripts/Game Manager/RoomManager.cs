3b2d7dee81bb0d297976d9590d386f532e177d3e
Simon Laursen <>
2018-03-24 12:05:14 +0100

    New graphics implemented

--- SpringCreateJam2018/Assets/Scripts/Game Manager/RoomManager.cs
+++ SpringCreateJam2018/Assets/Scripts/Game Manager/RoomManager.cs
@@ -10,18 +10,66 @@ public class RoomManager : MonoBehaviour {
      *  Missing function that implements the buttons to the panel
      *  in a grid formation.
      */
-
+    private RoomManager instance;
     public Rooms[] rooms;
+    public Fade fade;
+
+    public Image[] gridImg;
+    public Image[] aliveImg;
+    public Image[] deadImg;
 
     private bool lobbyRoom;
 
-	private void Update()
+	private void Awake()
 	{
-        CheckArea();
+        if(instance != null){
+            Destroy(gameObject);
+        }
+        else{
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            fade = GetComponent<Fade>();
+        }
 	}
 
+	private void Update()
+	{
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            CheckArea();
+            DrawGrid();
+        }
+	}
+
+    public void GridButton(int personNum){
+        if(!rooms[personNum].isDead){
+            fade.FadeTo(rooms[personNum].roomName);
+        }
+        else{
+            Debug.Log("Dead, can't go there");
+        }
+    }
+
+    public void GoBack(){
+        fade.FadeTo("Bellboy");
+    }
+
+    private void DrawGrid(){
+
+        for (int i = 0; i < gridImg.Length; i++){
+            if(gridImg[i] != null){
+                if(!rooms[i].isDead){
+                    gridImg[i] = aliveImg[i];
+                }
+                else{
+                    gridImg[i] = deadImg[i];
+                }
+            }
+        }
+    }
+
     private void CheckArea(){
-        if (SceneManager.GetActiveScene().name == "Bellboy")
+        if (SceneManager.GetActiveScene().name == "Bellboy" || SceneManager.GetActiveScene().name == "Choose")
         {
             lobbyRoom = true;
         }
